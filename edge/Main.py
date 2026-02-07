4cfb60b45e364246335d7cc6d8b0470a812dd870
KushSingh2805 <aryan1020ks@hotmail.com>
2026-02-07 21:25:48 +0530

    Edge Gateway Running

---
+++ edge/Main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class vestdata(BaseModel):
+	minerid:str
+	heartrate:int
+	gaslevel:int
+	moving:bool
+@app.get("/")
+def checkstatus():
+	return{"status":"5G Edge Node Online"}
+
+@app.post("/process")
+def logic(data:vestdata):
+	score=0
+
+	if data.gaslevel>100:
+		score+=5
+	elif data.gaslevel>50:
+		score+=2
+
+	if data.heartrate>130 or data.heartrate<50:
+		score+=4
+	elif data.heartrate>110:
+		score+=3
+
+
+	status="STABLE"
+
+	if score>=8:
+		status="CRITICAL"
+	elif score>=5:
+		status="WARNING"
+	print(f"Miner {data.minerid} status: {status} (Score:{score})")
+
+	return{
+		"Id":data.minerid, "Score":score, "Health":status, "Action":"DEPLOY DRONE" if status == "CRITICAL" else "MONITOR"}
+	
