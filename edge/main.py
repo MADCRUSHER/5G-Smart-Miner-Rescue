b9fb63fcfd0ee8b51a048b99c395c207b7a3d335
KushSingh2805 <aryan1020ks@hotmail.com>
2026-02-08 05:19:38 +0530

    injections value adjusted to arduino readings

--- edge/main.py
+++ edge/main.py
@@ -16,73 +16,53 @@ def checkstatus():
 	return{"status":"5G Edge Node Online"}
 
 @app.post("/process")
-def logic(data: vestdata):
-    global alert
+def logic(data:vestdata):
+	global alert
 
-    # RAW ADC values (0–1023)
-    hr_raw = data.heartrate
-    gas_raw = data.gaslevel
-    temp = data.temp  # already ~36–47
+	hrate=data.heartrate
+	gas=data.gaslevel
+	temp=data.temp
+	score=0
 
-    score = 0
+	if gas>=750:
+		score+=6
+	elif gas>=700:
+		score+=3
 
-    # ========== GAS (PRIMARY INDICATOR) ==========
-    # Typical raw gas values you observed: ~90–120 (can rise)
-    if gas_raw >= 700:
-        score += 6          # extreme gas
-    elif gas_raw >= 500:
-        score += 4          # high gas
-    elif gas_raw >= 300:
-        score += 2          # mild anomaly
+	if hrate>=1000:
+		score+=2
+	elif hrate<=905:
+		score=10
 
-    # ========== HEART RATE (RAW ADC) ==========
-    # Floating / high raw means stress / bad contact
-    if hr_raw >= 800:
-        score += 3
-    elif hr_raw >= 600:
-        score += 2
-    elif hr_raw >= 400:
-        score += 1
+	if temp>=41.5:
+		score+=6
+	elif temp>=39.5:
+		score+=3
+	elif temp<=37.2:
+		score=10
 
-    # ========== TEMPERATURE ==========
-    # Your formula already gives ~36.5–46
-    if temp >= 42:
-        score += 3
-    elif temp >= 39:
-        score += 2
-    elif temp >= 37.5:
-        score += 1
 
-    # ========== FINAL SEVERITY ==========
-    if score >= 8:
-        status = "CRITICAL"
-    elif score >= 4:
-        status = "WARNING"
-    else:
-        status = "STABLE"
+	
+	def status(score):
+		if score==10:
+			return"DECEASED"
+		elif score>=8:
+			return"CRITICAL"
+		elif score>=5:
+			return"MONITOR"
+		else:
+			return"STABLE"
+	booga=status(score)
 
-    alert = {
-        "id": data.minerid,
-        "category": status,
-        "score": score,
-        "heartrate_raw": hr_raw,
-        "gaslevel_raw": gas_raw,
-        "temp": temp,
-        "timestamp": "Real-time 5G feed"
-    }
+	miners=[]
+	miners.append({"id":data.minerid,"category":booga,"heartrate":hrate,"gas level":gas,"Temp":temp})
+	miners.append({"id":"Miner 102","category":"WARNING","heartrate":int(hrate*0.75),"gas level":int(gas*0.6),"Temp":round(temp-1.5,1)})
+	miners.append({"id":"Miner 103","category":"MONITOR","heartrate":int(hrate*0.4),"gas level":int(gas*0.3),"Temp":round(temp-3,1)})
+	miners.append({"id":"Miner 104","category":"STABLE","heartrate":int(hrate*0.35),"gas level":int(gas*0.25),"Temp":round(temp-2.5,1)})
 
-    print(
-        f"{data.minerid} | "
-        f"HR_RAW={hr_raw}, GAS_RAW={gas_raw}, TEMP={temp:.1f} → {status}"
-    )
-
-    return alert
-
+	alert ={ "miners":miners,"timestamp":"Real-time 5G feed"}
+	return alert
 
 @app.get("/status")
 def getstatus():
 	return alert
-
-
-
-
